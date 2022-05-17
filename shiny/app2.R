e86aad04d23ddf02e19997166399fdb79ae1eb00
HangyiWu <102491529+HangyiWu@users.noreply.github.com>
2022-05-18 00:25:43 +0800

    Update app2.R
    update ui and sever

--- shiny/app2.R
+++ shiny/app2.R
@@ -13,15 +13,16 @@ library(tidyverse)
 library(shinydashboard)
 
 load("shiny.rdata")
-#final_model <- lr_fitall
+#final_model <- final_model
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
-    dashboardHeader(title = "Basic dashboard"),
+    dashboardHeader(title = "kidney transplantation"),
     dashboardSidebar(
         sidebarMenu(
             menuItem("Introduction", tabName = "introduction"),
-            menuItem("Preidction", tabName = "preidction")
+            menuItem("Gene Summarize", tabName = "gene_summarize"),
+            menuItem("Prediction", tabName = "prediction")
         )
     ),
     dashboardBody(
@@ -29,9 +30,22 @@ ui <- dashboardPage(
             # First tab content
             tabItem(tabName = "introduction",
                     h2("Introduction"),
-                    p("This is a risk calculator about kidney transplantation, which can predict the success of kidney transplantation by inputting age, gender and other variables. Its main targeted users are doctors, which can help doctors accurately predict the results of kidney transplantation."),
+                    p("This is a risk calculator for kidney transplantation that predicts the success of a kidney transplant by entering age, gender and other variables. The main target users are doctors, who can fill in patient information and data to help them accurately predict the outcome of their kidney transplant."),
                     br(),
                     br(),
+                    
+                    h2("Notes"),
+                    h4("Age: "),
+                    p("The age range is 18-68 years old, as the age range of our training set is only 18-68 years old, we will collect more data in the future to expand the age range so that we can get more accurate prediction results for people beyond this age range."),
+                    h4("Sex: "),
+                    p("The data in our training set is only binary gender, so unfortunately we are unable to provide a non-binary gender option here and will need to collect further data to expand our options."),
+                    h4("Gene:"),
+                    p("The default value is the average value of the genes in the database.If the doctor does not know the patient's NM_000544 and NM_004803 gene data or is unable to fill in these two options, the average of these two genes in the database will be taken automatically instead."),
+           
+            ),
+            
+            # Seconed tab content
+            tabItem(tabName = "gene_summarize",
                     h2("Gene Summarize"),
                     p("The tissue-specific pattern of mRNA expression can indicate important clues about gene function. High-density oligonucleotide arrays offer the opportunity to examine patterns of gene expression on a genome scale. Toward this end, we have designed custom arrays that interrogate the expression of the vast majority of protein-encoding human and mouse genes and have used them to profile a panel of 79 human and 61 mouse tissues. The resulting data set provides the expression patterns for thousands of predicted genes, as well as known and poorly characterized genes, from mice and humans. We have explored this data set for global trends in gene expression, evaluated commonly used lines of evidence in gene prediction methodologies, and investigated patterns indicative of chromosomal organization of transcription. We describe hundreds of regions of correlated transcription and show that some are subject to both tissue and parental allele-specific expression, suggesting a link between spatial expression and imprinting."),
                     br(),
@@ -47,28 +61,50 @@ ui <- dashboardPage(
                     p("Gene Symbol: UCK2"),
                     p(strong("Uridine-cytidine kinase 2 (UCK2)"),"is an enzyme that in humans is encoded by the UCK2 gene."),
                     p("The protein encoded by this gene catalyzes the phosphorylation of uridine and cytidine to uridine monophosphate (UMP) and cytidine monophosphate (CMP), respectively. This is the first step in the production of the pyrimidine nucleoside triphosphates required for RNA and DNA synthesis. In addition, an allele of this gene may play a role in mediating nonhumoral immunity to Hemophilus influenzae type B."),
-                    br(),
-                    br(),
-                    br(),
-                    h2("CV accuracy"),
-                    p("The overall cv accuracy is 0.76."),
-                    p("The cv accuracy of GLM(generalized linear model) model is a little over 0.825."),
-                    p("The cv accuracy of RF(random forest) model is a little over 0.84.")
                     
             ),
             
-            # Second tab content
-            tabItem(tabName = "preidction",
+            
+            # Third tab content
+            tabItem(tabName = "prediction",
                     fluidRow(
-                      box("If the patient is non-binary", br(), "There is no option here",
-                          selectInput('sex', 'Select Sex', unique(combined_df_unscaled$sex))),
-                      box(numericInput('age', 'Select Age', '0', '100','1')),
-                      box(numericInput('NM_000544', 'Select NM_000544', '0', '100','1')),
-                      box(numericInput('NM_004803', 'Select NM_004803', '0', '100','1'))
-                    ),
-                    mainPanel(
-                      textOutput('outcome')
-                    )
+                      column(width = 12,
+                             box(
+                               title = "Inputs", status = "danger", width = NULL,
+                               radioButtons("sex", "Select Sex:",
+                                            c("Male", "Female")),
+                              numericInput("age", "Age:",
+                                           min = 18,
+                                           max = 68,
+                                           value = NULL
+                                 ),
+                              numericInput("NM_000544",
+                                           "NM_000544 (optional):",
+                                           min = 3,
+                                           max = 5,
+                                           value = 4.087559
+                                 ),
+                              numericInput("NM_004803",
+                                           "NM_004803 (optional):",
+                                           min = 4,
+                                           max = 6,
+                                           value = 4.827927
+                               ),
+                               )
+                             ) 
+                      ),
+                      
+                      column(width = 12,
+                             box(
+                               title = "Prediction", status = "warning", width = NULL,
+                               htmlOutput("modelPrediction"),
+                               br(),
+                               h5("CV accuracy: "),
+                               p("The overall cv accuracy is 0.76."),
+                               p("The cv accuracy of GLM(generalized linear model) model is a little over 0.825."),
+                               p("The cv accuracy of RF(random forest) model is a little over 0.84.")
+                             )
+                          )
             )
         )
     )
@@ -76,15 +112,18 @@ ui <- dashboardPage(
 
 
 server <- function(input, output) {
-  output$outcome <- renderText({
-    combined_df_unscaled$outcome %>%
-      filter(
-        sex == input$sex,
-        age == input$age,
-        NM_000544 == input$NM_000544,
-        NM_004803 == input$NM_004803
-      )
-    print(paste("Your outcome is", outcome))
+  output$modelPrediction <- shiny::renderUI({
+    input_data <- data.frame(
+      sex = input$sex,
+      age = input$age,
+      NM_000544 = input$NM_000544,
+      NM_004803 = input$NM_004803
+    )
+    # calculation
+    predicted_outcome <- predict(final_model, input_data)
+    
+    HTML(sprintf(strong("The predicted kidney transplantation result for given inputs is <strong>%.2f%%</strong>.")
+    ))
   })
 }
 
